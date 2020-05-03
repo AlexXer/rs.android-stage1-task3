51660ff2923cd04ff025eb58fa5e66648ec7d2f4
AlexXer <alexxerman@gmail.com>
2020-05-03 19:42:32 +0300

    feat: add SquareDecomposer task solution

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,9 +1,38 @@
 package subtask4
 
+import kotlin.math.floor
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return decomposeValue(number * number, number - 1)?.toTypedArray()
+    }
+
+    private fun decomposeValue(value: Int, first: Int): ArrayList<Int>? {
+        for( i in first downTo 1 ) {
+            val square = i * i
+            val reminder = value - square
+            if(reminder == 0 ) {
+                return arrayListOf(i)
+            }
+
+            if(reminder < 0 ) {
+                return null
+            }
+
+            var sqrt = floor( sqrt(reminder.toDouble()) ).toInt()
+            if(sqrt >= i) {
+                sqrt = i - 1
+            }
+            val decomposed = decomposeValue(reminder, sqrt)
+            if(decomposed != null) {
+                decomposed.add(i)
+                return decomposed
+            }
+        }
+        return null
     }
 }
