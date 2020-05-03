27663a3c7178e1c7c015264302f5781c4b45c7a7
AlexXer <alexxerman@gmail.com>
2020-05-03 11:40:31 +0300

    feat: add Combinator task solution

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -1,9 +1,22 @@
 package subtask2
 
+import java.math.BigInteger
+
 class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val m = array[0]
+        val n = array[1]
+        var res = 0
+        while (countCombinations(res,n)!=BigInteger.valueOf(m.toLong()) && res<n){
+            res++
+        }
+        return if (res>=n) null else res
     }
+
+
+    private fun fact(i:Int):BigInteger = if (i>=1) BigInteger.valueOf(i.toLong())*fact(i-1) else BigInteger.ONE
+
+    private fun countCombinations(m:Int, n:Int):BigInteger=fact(n)/(fact(m)*fact(n-m))
 }
